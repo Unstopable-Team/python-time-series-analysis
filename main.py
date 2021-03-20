6a1d0136cde4f80fc0f32c5185037a27440f7feb
Benjamin Wolba <mail@benjaminwolba.com>
2021-03-20 05:55:29 +0100

    Started implementation of ensoe api

--- main.py
+++ main.py
@@ -4,13 +4,18 @@
 # =============================================================================
 
 import numpy as np 
+import pandas as pd 
+
 import wapi
+from entsoe import EntsoeRawClient
 
 
 # =============================================================================
 # Get Data from Volue
+# API Wrapper: https://github.com/wattsight/wapi-python
 # =============================================================================
 
+"""
 config_file_path = 'config.ini'
 session = wapi.Session(config_file=config_file_path)
 
@@ -22,3 +27,26 @@ ts = curve.get_data(data_from='2018-01-01T14:00Z', data_to='2018-02-01T14:00Z')
 
 df = ts.to_pandas()
 print(df.head())
+#"""
+
+
+# =============================================================================
+# Get Data from Entso-E
+# API Wrapper: https://github.com/EnergieID/entsoe-py
+# =============================================================================
+
+#"""
+
+# Does not work yet: TypeError: iteration over a 0-d array
+
+key = np.loadtxt('entsoe.txt', dtype=str)
+
+client = EntsoeRawClient(api_key=key)
+
+start = pd.Timestamp('2018-01-01T14:00Z', tz='Europe/Brussels')
+end = pd.Timestamp('2018-02-01T14:00Z', tz='Europe/Brussels')
+country_code = 'BE'  
+client.query_day_ahead_prices(country_code, start, end)
+
+#"""
+
