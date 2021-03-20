3b19ac8bfb4663352fb81a02bca218070eddb940
Benjamin Wolba <mail@benjaminwolba.com>
2021-03-20 05:38:35 +0100

    Obtained First Curve

--- main.py
+++ main.py
@@ -20,3 +20,5 @@ curve = session.get_curve(name='pri de intraday €/mwh cet min15 a')
 
 ts = curve.get_data(data_from='2018-01-01T14:00Z', data_to='2018-02-01T14:00Z')
 
+df = ts.to_pandas()
+print(df.head())
