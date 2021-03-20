d0a3b3244de87e0852855cba18ee93746fdcc08a
Benjamin Wolba <mail@benjaminwolba.com>
2021-03-20 05:33:51 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,22 @@
+
+# =============================================================================
+# Import of Packages
+# =============================================================================
+
+import numpy as np 
+import wapi
+
+
+# =============================================================================
+# Get Data from Volue
+# =============================================================================
+
+config_file_path = 'config.ini'
+session = wapi.Session(config_file=config_file_path)
+
+
+curve = session.get_curve(name='pri de intraday €/mwh cet min15 a')
+# print(curve.curve_type)
+
+ts = curve.get_data(data_from='2018-01-01T14:00Z', data_to='2018-02-01T14:00Z')
+
